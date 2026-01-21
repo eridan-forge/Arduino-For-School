9c301c5fb934554e2213e08245d038b614085d59
froge-people <eridanforge@gmail.com>
2026-01-21 15:35:41 +0300

    Create blink.ino
    Добавил первый урок: мигающий светодиод

---
+++ lessons/01-blink-led/blink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(13, OUTPUT); // Светодиод на пине 13
+  pinMode(LED_BUILTIN, OUTPUT); // Добавляем светодиод
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // Включить
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Ждать 1 секунду
+  digitalWrite(13, LOW); // Выключить
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // 1000 миллисекунд это 1 секунда 
+}
