019abad66dfbe6566f715eadb4a69848b966d80c
froge-people <eridanforge@gmail.com>
2026-01-21 15:43:57 +0300

    Update blink.ino

--- lessons/01-blink-led/blink.ino
+++ lessons/01-blink-led/blink.ino
@@ -11,3 +11,5 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000); // 1000 миллисекунд это 1 секунда 
 }
+
+[Посмотреть готовый результат в симуляторе wowki](https://wokwi.com/projects/453758956373015553)
