3f19acec9e848a0364bd5605f58d60441e6c6bec
froge-people <eridanforge@gmail.com>
2026-01-25 21:23:58 +0300

    Update button_hold.ino

--- lessons/03-button-and-led/03-button-hold/button_hold.ino
+++ lessons/03-button-and-led/03-button-hold/button_hold.ino
@@ -1,1 +1,27 @@
+/*
+  Урок 3.3: Кнопка с удержанием
+  Светодиод горит только пока держите кнопку
+*/
 
+#define BUTTON_PIN 2
+#define LED_PIN 13
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println("Удерживай кнопку");
+  Serial.println("Держите кнопку - светодиод горит");
+}
+
+void loop() {
+  if (digitalRead(BUTTON_PIN) == LOW) { // ПОСЛЕ if вседа ()
+    digitalWrite(LED_PIN, HIGH); // Включаем при нажатии светодиоид
+  } else {
+    digitalWrite(LED_PIN, LOW); // Включаем при отпускании
+  }
+
+  delay(50); // Небольшая задержка
+
+}
