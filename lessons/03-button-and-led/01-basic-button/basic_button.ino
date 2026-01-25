99192d53761e8596acc1bc8ea87f845378824fe5
froge-people <eridanforge@gmail.com>
2026-01-25 20:34:22 +0300

    Update basic_button.ino

--- lessons/03-button-and-led/01-basic-button/basic_button.ino
+++ lessons/03-button-and-led/01-basic-button/basic_button.ino
@@ -1,1 +1,28 @@
+/*
+  Урок 3.1: Простейшая кнопка (без защиты от дребезга)
+  Показывает проблему множественных срабатываний
+*/
 
+#define BUTTON_PIN 2   // Кнопка на D2
+#define LED_PIN 13     // Светодиод на D13 (встроенный)
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);  // Кнопка с подтягивающим резистором
+  pinMode(LED_PIN, OUTPUT);           // Светодиод как выход
+  
+  Serial.begin(9600);
+  Serial.println("Базовая кнопка (проблема дребезга)");
+}
+
+void loop() {
+  // ПРОСТАЯ проверка кнопки (есть проблема!)
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    // Переключаем светодиод
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    
+    Serial.println("Кнопка нажата!");
+    
+    // Маленькая задержка, но не решает проблему полностью
+    delay(100);
+  }
+}
